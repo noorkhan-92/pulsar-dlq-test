2c8d32c3a1c71009ebe3419d8ec260cb20373338
Hamza Azam <im.hamzaazam@gmail.com>
2023-02-03 17:50:21 +0500

    Adding spring project

---
+++ src/test/kotlin/global/din/pulsartest/PulsarTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package global.din.pulsartest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PulsarTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
