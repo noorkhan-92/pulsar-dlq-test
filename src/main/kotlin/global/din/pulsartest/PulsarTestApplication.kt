5071292903084d7134c4b089e3927b9dd7d462f1
Noor Khan <noorkhan_92@hotmail.com>
2023-03-22 13:24:24 +0500

    dlq test

--- src/main/kotlin/global/din/pulsartest/PulsarTestApplication.kt
+++ src/main/kotlin/global/din/pulsartest/PulsarTestApplication.kt
@@ -1,53 +1,25 @@
 package global.din.pulsartest
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.stream.function.StreamBridge
 import org.springframework.context.annotation.Bean
 import org.springframework.messaging.Message
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import java.util.function.Consumer
-import java.util.function.Function
-import java.util.function.Supplier
 
 @SpringBootApplication
 class PulsarTestApplication() {
 
-
-//    @Bean
-//    fun timeSupplier(): Supplier<Time> {
-//        return Supplier { Time(System.currentTimeMillis().toString()) }
-//    }
-
     @Bean
-    fun timeProcessor(): Consumer<Message<Time>> = Consumer {
-        message ->
-        val temp = message.payload
-        println(temp)
+    fun testListener(): Consumer<Message<TestMessage>> = Consumer {
+            message ->
+        println(message.payload)
+        return@Consumer
     }
 
     @JvmRecord
-    data class Time(val time: String)
-
-    @JvmRecord
-    data class EnhancedTime(val time: Time, val extra: String) {}
-
-    @RestController
-    class MessageResource(var bridge: StreamBridge) {
-        @GetMapping("/")
-        fun index(): String{
-            bridge.send("timeSupplier-out-0",  Time(System.currentTimeMillis().toString()))
-            return "String"
-        }
-    }
-
+    data class TestMessage(val message: String)
 }
 
 fun main(args: Array<String>) {
     runApplication<PulsarTestApplication>(*args)
-
 }
--- src/test/kotlin/global/din/pulsartest/PulsarTestApplicationTests.kt
+++ src/test/kotlin/global/din/pulsartest/PulsarTestApplicationTests.kt
@@ -1,13 +1,19 @@
 package global.din.pulsartest
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cloud.stream.function.StreamBridge
 
 @SpringBootTest
 class PulsarTestApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Autowired
+    lateinit var bridge: StreamBridge
+	
+    @Test
+    fun dlqTest() {
+        bridge.send("test", PulsarTestApplication.TestMessage("pulsar test message"))
+        Thread.sleep(10000)
+    }
 }
